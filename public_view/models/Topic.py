8964ff4621a014c92fc36cb77a1ade44843d7505
humanbeing <egeatmaca98@gmail.com>
2022-12-08 20:57:03 +0100

    index template created

--- public_view/models/Topic.py
+++ public_view/models/Topic.py
@@ -1,10 +1,10 @@
 from datetime import datetime as dt
 
 class Topic:
-    def __init__(self, id, title, content, user_id):
+    def __init__(self, id, title, description, user_id):
         self.id = id
         self.title = title
-        self.content = content
+        self.description = description
         self.comments = []
         self.user_id = user_id
         self.created_at = dt.now()
--- public_view/views/index.py
+++ public_view/views/index.py
@@ -7,9 +7,24 @@ def index(request):
         'index.html', 
         {
             'topics': [
-                Topic(0, 'Some Sustainability Topic', 'This topic is mentioning very important problems regarding life under water.', 0),
-                Topic(0, 'Another Topic', 'Lorem ipsum lorem ipsum lorem ipsum.', 0),
-                Topic(0, 'Another Topic', 'Lorem ipsum lorem ipsum lorem ipsum.', 0)
+                Topic(0, 'Some Sustainability Topic',
+                      'This topic is mentioning very important problems regarding life under water.', 0),
+                Topic(0, 'Another Topic',
+                      'Lorem ipsum lorem ipsum lorem ipsum.', 0),
+                Topic(0, 'Another Topic',
+                      'Lorem ipsum lorem ipsum lorem ipsum.', 0),
+                Topic(0, 'Some Sustainability Topic',
+                      'This topic is mentioning very important problems regarding life under water.', 0),
+                Topic(0, 'Another Topic',
+                      'Lorem ipsum lorem ipsum lorem ipsum.', 0),
+                Topic(0, 'Another Topic',
+                      'Lorem ipsum lorem ipsum lorem ipsum.', 0),
+                Topic(0, 'Some Sustainability Topic',
+                      'This topic is mentioning very important problems regarding life under water.', 0),
+                Topic(0, 'Another Topic',
+                      'Lorem ipsum lorem ipsum lorem ipsum.', 0),
+                Topic(0, 'Another Topic',
+                      'Lorem ipsum lorem ipsum lorem ipsum.', 0),
             ]
         }
     )
