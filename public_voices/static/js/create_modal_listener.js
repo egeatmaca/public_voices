6b9c165b96a40f8cb17c18feea284015c17ef52d
humanbeing <egeatmaca98@gmail.com>
2022-12-09 01:45:44 +0100

    close modal by outside click

--- public_voices/static/js/create_modal_listener.js
+++ public_voices/static/js/create_modal_listener.js
@@ -6,3 +6,16 @@ document.querySelector('.btn_create').addEventListener('click', function(e) {
     modal_container.style.width = "100%";
     modal_container.style.height = "100%";
 });
+
+document
+  .querySelector(".modal_container")
+  .addEventListener("click", function (e) {
+    if (e.target.closest(".modal_bottom")) return;
+
+    var modal_container = document.querySelector(
+      ".create_modal .modal_container"
+    );
+    modal_container.style.display = "none";
+    modal_container.style.width = "0";
+    modal_container.style.height = "0";
+  });
