8ef65273bac71f09faf9daf5f2f6512486920924
humanbeing <egeatmaca98@gmail.com>
2022-12-18 10:53:23 +0100

    TopicAnalyzer:get_word_counts

--- public_voices/TopicAnalyzer.py
+++ public_voices/TopicAnalyzer.py
@@ -17,14 +17,11 @@ class TopicAnalyzer:
 
     def get_word_counts(self):
         comment_contents = pd.Series(self.comments).apply(lambda x: x['content'])
-        print(comment_contents)
         self.vectorizer = CountVectorizer(
             stop_words='english').fit(comment_contents)
         self.comment_vectors = pd.DataFrame(self.vectorizer.transform(
-            comment_contents), columns=self.vectorizer.get_feature_names())
-        print(self.comment_vectors)
+            comment_contents).toarray(), columns=self.vectorizer.get_feature_names_out())
         self.word_counts = self.comment_vectors.sum(axis=0).sort_values(ascending=False)
-        print(self.word_counts)
         return self.word_counts
 
 if __name__ == '__main__':
